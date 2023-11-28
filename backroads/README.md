9f632e01a0528007e01dad1b46c26e17cfd41bf1
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-11-28 09:51:24 +0900

    컴포넌트로 분리

--- backroads/README.md
+++ backroads/README.md
@@ -1,3 +1,5 @@
 # React + Vite
 
 # HTML CSS JS 사이트 => 리엑트로 변환
+
+## 1. 컴포넌트로 분리
---
+++ backroads/src/data.js
@@ -0,0 +1,16 @@
+import tour1 from "/images/tour-1.jpeg";
+import tour2 from "/images/tour-2.jpeg";
+import tour3 from "/images/tour-3.jpeg";
+import tour4 from "/images/tour-4.jpeg";
+
+export const pageLinks = [
+  { id: 1, href: "#home", text: "home" },
+  { id: 2, href: "#about", text: "about" },
+  { id: 3, href: "#services", text: "services" },
+  { id: 4, href: "#tours", text: "tours" },
+];
+export const socialLinks = [
+  { id: 1, href: "https://www.twitter.com", icon: "fab fa-facebook" },
+  { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+  { id: 3, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
+];
